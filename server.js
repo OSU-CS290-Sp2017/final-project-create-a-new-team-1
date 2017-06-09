95acc262f36328038df4931377524b6b3aeb4144
Johnathan Lee <leejohna@flip3.engr.oregonstate.edu>
2017-06-08 18:49:34 -0700

    furthered server code

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
 var port = process.env.PORT || 3000;
+var entryData = require('entryData');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.get('/', function(req, res, next){
-  next();
-}
+	var entry = entryData;
+	if(entry){
+		var templateArgs = {
+			entries: entry,
+			show: true
+		}
+		res.render('entryPage', templateArgs);
+	}
+	else{
+		next();
+	}
+});
+
 app.get('*', function(req, res){
   res.status(404).render('404');
 });
